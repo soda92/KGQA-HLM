c88ac58b49aad9f500263f76068bc00c8755dec5
SodaCris <18463922396@163.com>
2021-04-28 18:49:06 +0800

    format

--- spider/show_profile.py
+++ spider/show_profile.py
@@ -3,11 +3,11 @@ import json
 with open('./spider/json/data.json', encoding='utf-8')as f:
     data = json.load(f)
 
-def get_profile(name):
-    s=''
-    for i in data[name]:
-        st="<dt class = \"basicInfo-item name\" >"+ str(i)+" \
-        <dd class = \"basicInfo-item value\" >"+str(data[name][i])+"</dd >"
-        s+=st
-    return s
 
+def get_profile(name):
+    s = ''
+    for i in data[name]:
+        st = "<dt class = \"basicInfo-item name\" >" + str(i)+" \
+        <dd class = \"basicInfo-item value\" >"+str(data[name][i])+"</dd >"
+        s += st
+    return s
